a2026af51c25951c3490d12a4ffd12a1f52da36d
lucasKaoid <lucas.lmspereira@gmail.com>
2023-01-21 10:19:27 -0300

    plot hex mesh with pkg 'hexalattice'

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import numpy
+import matplotlib.pyplot as plt
+from hexalattice.hexalattice import *
+
+hex_centers, _ = create_hex_grid(nx=5,
+                                 ny=5,
+                                 do_plot=True)
+                                 
+plt.show()

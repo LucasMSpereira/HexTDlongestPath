9d94059f0b2458e77cf9de8d90dd6dee144e9174
Lucas Pereira <lucas.lmspereira@gmail.com>
2023-03-15 16:47:52 -0300

    final touches in main script

--- NN.py
+++ NN.py
@@ -1,3 +1,9 @@
+# This file includes the main steps in the DNN application:
+  # split and batch data
+  # hyperparameter and architecture search
+  # retrain with earlystopping and logging
+  # load trained models
+  # visually compare outputs and labels
 #%% Imports
 import utilities
 import numpy as np
@@ -16,102 +22,100 @@ ds = utilities.dataManager(0, rowAmount, colAmount)
 # goal = "optimalPath"
 goal = "OSPlength"
 #%% Data
-# dataTrain, dataVal = ds.TFdata(goal) # data splits
-# batchSize = 256
-# # organize both splits in batches
-# batchesTrain = dataTrain.batch(batch_size = batchSize,
-#   num_parallel_calls = tf.data.AUTOTUNE, deterministic = False
-# )
-# batchesVal = dataVal.batch(batch_size = batchSize,
-#   num_parallel_calls = tf.data.AUTOTUNE, deterministic = False
-# )
-# # %% Hyperparameter optimization
-# def build_model(hp):
-#   inputs = keras.Input(shape = (rowAmount * colAmount,), name = "initialMap")
-#   # HP search defines number of hidden layers and their size
-#   layerSize = hp.Int("units", min_value = 32, max_value = 256, step = 64)
-#   numberOfLayers = hp.Int("nLayers", min_value = 2, max_value = 8, step = 2)
-#   x = layers.Dense(layerSize, activation = "relu")(inputs)
-#   for _ in range(1, numberOfLayers):
-#     x = layers.Dense(layerSize, activation = "relu")(x)
-#   if goal == "OSPlength": # if predicting length of OSP
-#     outputs = layers.Dense(1, name = goal)(x)
-#   elif goal == "optimalPath": # if predicting OSP
-#     outputs = layers.Dense(rowAmount * colAmount, name = goal)(x)
-#   model = keras.Model(
-#     inputs = inputs, outputs = outputs, name = "DNN_model"
-#   )
-#   model.compile(
-#     optimizer = keras.optimizers.RMSprop(
-#     learning_rate = hp.Float("lr", min_value = 1e-4,
-#       max_value = 1, sampling = "log")
-#     ),
-#     loss = keras.losses.MeanSquaredError(),
-#     metrics = [keras.metrics.MeanAbsoluteError()]
-#   )
-#   return model
-# #%% Keras tuner API
-# tuner = keras_tuner.BayesianOptimization(
-#     build_model,
-#     objective = 'val_loss',
-#     directory = "logs\\DNN\\OSP_length" if goal == "OSPlength" else "logs\\DNN\\OptMap",
-#     project_name = "HPopt",
-#     max_trials = 20
-# )
-# # Perform hyperparameter search
-# tuner.search(batchesTrain, epochs = 30, validation_data = batchesVal)
-# # List ranked hyperparameter combinations
-# tuner.results_summary()
-# #%% Train model from scratch with best hyperparameters
-# # Early-stop callback
-# earlyStopCallback = keras.callbacks.EarlyStopping(
-#     monitor = 'val_loss', min_delta = 0, patience = 20, verbose = 1,
-#     mode = 'auto', baseline = None, restore_best_weights = True,
-# )
-# # Log folder for tensorboard
-# if goal == "OSPlength":
-#   tbPath = "logs\\DNN\\OSP_length\\" + str(len(list(WindowsPath("./logs/DNN/OSP_length").iterdir())) + 1)
-# elif goal == "optimalPath":
-#   tbPath = "logs\\DNN\\OptMap\\" + str(len(list(WindowsPath("./logs/DNN/OSP_length").iterdir())) + 1)
-# tensorBoardCallback = keras.callbacks.TensorBoard(
-#     log_dir = tbPath,
-#     histogram_freq = 1, write_graph = True, write_images = True,
-#     write_steps_per_second = True, update_freq = 'epoch'
-# )
-# # Model architecture
-# inputs = keras.Input(shape = (rowAmount * colAmount,), name = "initialMap")
-# hiddenSize = 32 if goal == "OSPlength" else 224
-# if goal == "OSPlength":
-#   x = layers.Dense(hiddenSize, activation = "relu")(inputs)
-# elif goal == "optimalPath":
-#   x = layers.Dense(hiddenSize, activation = "relu")(inputs)
-# hiddenL = 1 if goal == "OSPlength" else 7
-# for _ in range(hiddenL):
-#   x = layers.Dense(hiddenSize, activation = "relu")(x)
-# if goal == "OSPlength":
-#   outputs = layers.Dense(1, name = goal)(x)
-# elif goal == "optimalPath":
-#   outputs = layers.Dense(rowAmount * colAmount, name = goal)(x)
-# restartBestModel = keras.Model(
-#   inputs = inputs, outputs = outputs, name = "DNN_model"
-# )
-# restartBestModel.summary()
-# restartBestModel.compile(
-#     optimizer = keras.optimizers.RMSprop(learning_rate = 0.0001),
-#     loss = keras.losses.MeanSquaredError(),
-#     metrics = [keras.metrics.MeanAbsoluteError()]
-# )
-# # Train model
-# restartBestModel.fit(
-#   batchesTrain, epochs = 100, callbacks = [earlyStopCallback, tensorBoardCallback],
-#   verbose = 1, validation_data = batchesVal
-# )
-# #%% Save model with best hyperparameters trained with early-stop
-# if goal == "OSPlength":
-#   modelPath = 'logs\\DNN\\OSP_length\\DNN_OSP_length.h5'
-# elif goal == "optimalPath":
-#   modelPath = 'logs\\DNN\\OptMap\\DNN_OSP.h5'
-# restartBestModel.save(modelPath)
+dataTrain, dataVal = ds.TFdata(goal) # data splits
+batchSize = 256
+# organize both splits in batches
+batchesTrain = dataTrain.batch(batch_size = batchSize,
+  num_parallel_calls = tf.data.AUTOTUNE, deterministic = False
+)
+batchesVal = dataVal.batch(batch_size = batchSize,
+  num_parallel_calls = tf.data.AUTOTUNE, deterministic = False
+)
+# %% Hyperparameter optimization
+def build_model(hp):
+  inputs = keras.Input(shape = (rowAmount * colAmount,), name = "initialMap")
+  # HP search defines number of hidden layers and their size
+  layerSize = hp.Int("units", min_value = 32, max_value = 256, step = 64)
+  numberOfLayers = hp.Int("nLayers", min_value = 2, max_value = 8, step = 2)
+  x = layers.Dense(layerSize, activation = "relu")(inputs)
+  for _ in range(1, numberOfLayers):
+    x = layers.Dense(layerSize, activation = "relu")(x)
+  if goal == "OSPlength": # if predicting length of OSP
+    outputs = layers.Dense(1, name = goal)(x)
+  elif goal == "optimalPath": # if predicting OSP
+    outputs = layers.Dense(rowAmount * colAmount, name = goal)(x)
+  model = keras.Model(inputs = inputs, outputs = outputs, name = "DNN_model")
+  model.compile(
+    optimizer = keras.optimizers.RMSprop(
+    learning_rate = hp.Float("lr", min_value = 1e-4,
+      max_value = 1, sampling = "log")
+    ),
+    loss = keras.losses.MeanSquaredError(),
+    metrics = [keras.metrics.MeanAbsoluteError()]
+  )
+  return model
+#%% Keras tuner API
+tuner = keras_tuner.BayesianOptimization(
+    build_model,
+    objective = 'val_loss',
+    directory = "logs\\DNN\\OSP_length" if goal == "OSPlength" else "logs\\DNN\\OptMap",
+    project_name = "HPopt",
+    max_trials = 20
+)
+# Perform HP search
+tuner.search(batchesTrain, epochs = 30, validation_data = batchesVal)
+# Print list of ranked HP combinations
+tuner.results_summary()
+#%% Train model from scratch with best hyperparameters
+# Early-stop callback
+earlyStopCallback = keras.callbacks.EarlyStopping(
+    monitor = 'val_loss', min_delta = 0, patience = 20, verbose = 1,
+    mode = 'auto', baseline = None, restore_best_weights = True,
+)
+# Log folder for tensorboard
+if goal == "OSPlength":
+  tbPath = "logs\\DNN\\OSP_length\\" + str(len(list(WindowsPath("./logs/DNN/OSP_length").iterdir())) + 1)
+elif goal == "optimalPath":
+  tbPath = "logs\\DNN\\OptMap\\" + str(len(list(WindowsPath("./logs/DNN/OSP_length").iterdir())) + 1)
+tensorBoardCallback = keras.callbacks.TensorBoard(
+    log_dir = tbPath,
+    histogram_freq = 1, write_graph = True, write_images = True,
+    write_steps_per_second = True, update_freq = 'epoch'
+)
+# Model architecture
+inputs = keras.Input(shape = (rowAmount * colAmount,), name = "initialMap")
+hiddenSize = 32 if goal == "OSPlength" else 224
+if goal == "OSPlength":
+  x = layers.Dense(hiddenSize, activation = "relu")(inputs)
+elif goal == "optimalPath":
+  x = layers.Dense(hiddenSize, activation = "relu")(inputs)
+hiddenL = 1 if goal == "OSPlength" else 7
+for _ in range(hiddenL):
+  x = layers.Dense(hiddenSize, activation = "relu")(x)
+if goal == "OSPlength":
+  outputs = layers.Dense(1, name = goal)(x)
+elif goal == "optimalPath":
+  outputs = layers.Dense(rowAmount * colAmount, name = goal)(x)
+restartBestModel = keras.Model(
+  inputs = inputs, outputs = outputs, name = "DNN_model"
+)
+restartBestModel.summary()
+restartBestModel.compile(
+    optimizer = keras.optimizers.RMSprop(learning_rate = 0.0001),
+    loss = keras.losses.MeanSquaredError(),
+    metrics = [keras.metrics.MeanAbsoluteError()]
+)
+# Train model
+restartBestModel.fit(
+  batchesTrain, epochs = 100, callbacks = [earlyStopCallback, tensorBoardCallback],
+  verbose = 1, validation_data = batchesVal
+)
+#%% Save model with best hyperparameters trained with early-stop
+if goal == "OSPlength":
+  modelPath = 'logs\\DNN\\OSP_length\\DNN_OSP_length.h5'
+elif goal == "optimalPath":
+  modelPath = 'logs\\DNN\\OptMap\\DNN_OSP.h5'
+restartBestModel.save(modelPath)
 # %% Visualize results from best models
 # Load model that predicts OSP
 OSPmodel = tf.keras.models.load_model('logs\\DNN\\OptMap\\DNN_OSP.h5')
@@ -119,7 +123,7 @@ OSPmodel = tf.keras.models.load_model('logs\\DNN\\OptMap\\DNN_OSP.h5')
 OSPlengthModel = tf.keras.models.load_model('logs\\DNN\\OSP_length\\DNN_OSP_length.h5')
 ds = utilities.dataManager(0, rowAmount, colAmount)
 compareData = ds.TFdata("both") # dataset with both labels
-#%%
+#%% Visually compare labels and outputs from trained models
 for (initMapSample, OSPlengthSample, OSPsample) in compareData.take(5).batch(1):
   # use models in current sample
   map0 = np.asarray(initMapSample).reshape(-1, 1).transpose()
@@ -151,4 +155,3 @@ for (initMapSample, OSPlengthSample, OSPsample) in compareData.take(5).batch(1):
   Steps in initial map: {gm.totalSteps(gm.mapDefinition[0])}
   Steps in optimal map: {gm.totalSteps(gm.mapDefinition[1])}
   """)
-# %%

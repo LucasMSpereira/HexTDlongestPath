e0691068cf8aef9b3aca4b73d120de8c60a55c06
Lucas Pereira <lucas.lmspereira@gmail.com>
2024-01-01 23:04:05 -0300

    reorganize code in dglUtils.py

--- dglUtils.py
+++ dglUtils.py
@@ -1,18 +1,18 @@
-# definitions for implementing the graph transformer
-
+# definitions for implementing the graph convolutional network
 import dgl
 from dgl.nn import GraphConv
 from torch import nn
 
 class gcn(nn.Module):
-    
+
   """Graph convolutional network"""
 
   def __init__(self, params: dict) -> None:
     super(gcn, self).__init__()
-    
-    self.conv1 = GraphConv(params["inDim"], params["hDim"], activation = params["activFunction"])
-    self.conv2 = GraphConv(params["hDim"], 1)
+    self.conv1 = GraphConv(
+      params["inDim"], params["hDim"], activation = params["activFunction"]
+    )
+    self.conv2 = GraphConv(params["hDim"], params["hDim"])
 
   def forward(self, g, inFeature):
     h = self.conv1(g, inFeature)

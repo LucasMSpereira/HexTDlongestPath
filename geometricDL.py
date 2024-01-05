24f260fb75167e205d34d8ebdd5454a53c7d2bad
LucasMSpereira <lucas.lmspereira@gmail.com>
2024-01-04 23:15:01 -0300

    plotting in GCN script

--- geometricDL.py
+++ geometricDL.py
@@ -1,11 +1,14 @@
 #%% Imports
 import os
 os.environ['DGLBACKEND'] = 'pytorch'
+from datetime import datetime
+import matplotlib
 import dglUtils
 import random
 import data_utils
+from pandas import DataFrame as df
 import torch
-import dgl
+import seaborn as sns
 import torch.nn.functional as F
 random.seed(100)
 #%% Data
@@ -14,7 +17,7 @@ nNodes = rowAmount * colAmount
 ds = data_utils.dataManager(0, rowAmount, colAmount)
 bSize = 100
 trainBatchLoader, valBatchLoader = ds.readDGLdataset(
-  trainPercent = 0.05, batchSize = bSize
+  trainPercent = 0.7, batchSize = bSize
 )
 ba = next(iter(trainBatchLoader))
 print(ba)
@@ -25,14 +28,21 @@ model = dglUtils.gcn({
   "hDim": batchNumNodes,
   "activFunction": F.hardswish
 })
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
-lossHist = []
+optimizer = torch.optim.Adam(model.parameters(), lr = 1e-4)
+lossHist = df(columns = ['Batch', 'MSE loss'])
+dfPos = -1
 for epoch in range(2):
+  print(f"""Epoch {epoch + 1} {datetime.now().strftime("%H:%M:%S")}""")
   for batchID, (batched_graph, OSPlength) in enumerate(trainBatchLoader):
+    dfPos += 1
     pred = model(batched_graph, batched_graph.ndata["initialType"].float())
     loss = F.mse_loss(pred.float(), OSPlength.float())
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    lossHist.append(loss)
-    print(batchID + 1, f"{lossHist[-1].item():.3e}")
+    lossHist.loc[dfPos] = [batchID + 1, loss.item()]
+    if random.random() > 0.95:
+      print(f"Batch: {batchID + 1}   Loss: {lossHist.iloc[-1]['MSE loss']:.3e}")
+  facetgrid = sns.relplot(data = lossHist, kind = 'line', x = 'Batch', y = 'MSE loss')
+  facetgrid.ax.set_yscale("log")
+  matplotlib.pyplot.show()
